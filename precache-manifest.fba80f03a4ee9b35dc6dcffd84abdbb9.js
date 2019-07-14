8f9c5aec62032b9eb8ca63e723f5f6ff9003b7fd
Nancy <lexyfeer@hotmail.com>
2019-07-13 22:12:45 -0500

    Updates

---
+++ precache-manifest.fba80f03a4ee9b35dc6dcffd84abdbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb5222236899321cd0d1100ccbee344",
+    "url": "/frontend-burger-queen/index.html"
+  },
+  {
+    "revision": "2e18d2be9994f04eb61a",
+    "url": "/frontend-burger-queen/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/frontend-burger-queen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2e18d2be9994f04eb61a",
+    "url": "/frontend-burger-queen/static/js/main.1fa5beec.chunk.js"
+  },
+  {
+    "revision": "5afe00ae6bde01fdd42a",
+    "url": "/frontend-burger-queen/static/js/runtime~main.6d57efab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
