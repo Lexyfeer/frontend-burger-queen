c91c6565e137a80ab70a9624f7d93181ecaeb76f
Nancy <lexyfeer@hotmail.com>
2019-07-14 13:56:55 -0500

    Integre React-router-dom para realizar SPA y cree la barra de navegacion

---
+++ src/components/MakeOrders.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MakeOrders extends Component {
+    render() {
+            return(
+                <div>
+                    <p>Componente para tomar ordenes</p>
+                    
+                    
+                </div>
+            )
+        }
+    }
+
+export default MakeOrders;
